428dd1af2af0a4671ec6fb12a20e8f7b9135796a
Zbigniew Zasieczny <z.zasieczny@gmail.com>
2016-12-05 12:11:37 +0100

    added licensing information to the main file

--- desktop_lock_suspender.py
+++ desktop_lock_suspender.py
@@ -1,11 +1,24 @@
-#!/usr/bin/env python
 # Module     : session lock suspender program
 # Synopsis   : Windows System tray icon.
 # Programmer : Zbigniew Zasieczny - z.zasieczny@gmail.com
 # Date       : 1 December 2016
 # Notes      : Based on (i.e. ripped off from) Simon Brunning's
 #              SysTrayIcon.py
+"""
+Copyright [2016] [Zbigniew Zasieczny]
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 from SysTrayIcon import SysTrayIcon
 from threading import Thread
 import ctypes
